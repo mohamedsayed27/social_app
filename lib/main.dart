ec6ad5533a93210f1a2b325b04426ccebc40d539
dell <mohamedsayed27>
2022-06-17 20:32:32 +0200

    cash helper class done

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,15 @@
+import 'package:bloc/bloc.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:social_app/bloc_observer.dart';
+import 'package:social_app/data/local/cash_helper.dart';
 import 'package:social_app/presentation/screens/login.dart';
-
 import 'firebase_options.dart';
 
 void main() async{
+  Bloc.observer = MyBlocObserver();
   WidgetsFlutterBinding.ensureInitialized();
+  await CashHelper.init();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
